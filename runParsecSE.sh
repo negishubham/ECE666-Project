4070498ac9c09af9fa3bc15746cf9edcfab102fe
Subhav Ramachandran <subhav@purdue.edu>
2022-05-05 17:22:01 -0400

    Parameterize runParsecSE.sh

--- runParsecSE.sh
+++ runParsecSE.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+#
+# Environment variables that can be set:
+#   GEM5_NCORES
+#   GEM5_OPT
+#   BM_DATASET
+#   PARSECDIR
 
-gem5_nCores="16"
+gem5_nCores="${GEM5_NCORES:=4}"
 
 gem5_result_prefix="project/results"
-gem5_test_case="${gem5_nCores}-se-nohotfix-global-conf2-dev"
+bm_dataset="${BM_DATASET:=simsmall}"
+gem5_test_case="${gem5_nCores}-${bm_dataset}-se-mesi"
 
 # gem5 Settings
 gem5_dir="${PWD}"
-gem5_opt="build/ECE666/gem5.opt"
+gem5_opt="${GEM5_OPT:=${gem5_dir}/build/ECE666/gem5.opt}"
 gem5_config="configs/example/se.py"
 
 gem5_args="${gem5_dir}/${gem5_config} -n ${gem5_nCores} --ruby"
-gem5_exec="build/ECE666/gem5.opt ${gem5_args}"
+gem5_exec="${gem5_opt} ${gem5_args}"
 
 # TODO add args for ruby system and
 
 # PARSEC Settings
-bm_dataset="simdev"
+
+bm_dir="${PARSECDIR:=${gem5_dir}/../parsec-2.1}"
 
 declare -A bm_type
 
@@ -37,21 +45,22 @@ bm_type["streamcluster"]="kernels"
 
 bm_names=(
     "blackscholes"
-    "bodytrack"
-    "canneal"
-    "dedup"
-    "streamcluster"
-    "facesim"
-    "ferret"
-    "fluidanimate"
+    # "streamcluster"
+    # "fluidanimate"  # takes time ???, expects NTHREADS to be power of 2
     "freqmine"
-    "raytrace"
-    "swaptions"
-    "vips"
+    # "raytrace"      # requires n-1 threads
+
+    # "canneal"     # seems to take too long
+    # "dedup"         # args not populated
+    # "bodytrack"   # error when creating threads
+    # "facesim"       # args not populated
+    # "ferret"      #fatal msync unimplemented
+    # "swaptions"   # run args don't get populated
+    # "vips"        #syscall sched_getparam unimplemented
 )
 
 
-mkdir "${gem5_dir}/${gem5_result_prefix}/m5out-${gem5_test_case}"
+mkdir -p "${gem5_dir}/${gem5_result_prefix}/m5out-${gem5_test_case}"
 
 # bm_name="blackscholes"
 
@@ -60,10 +69,13 @@ for bm_name in "${bm_names[@]}"; do
     echo ${bm_name}
     # echo ${bm_type[${bm_name}]}
 
-    bm_dir="${gem5_dir}/benchmarks/parsec-2.1"
     bm_subdir="pkgs/${bm_type[${bm_name}]}/${bm_name}"
     bm_build="inst/amd64-linux.gcc.pre" #/bin/${bm_name}"
-    bm_NTHREADS=$((${gem5_nCores} - 1))
+    bm_NTHREADS=${gem5_nCores}
+    if [ bm_name == "raytrace" ]; then
+        bm_NTHREADS=$((${gem5_nCores} - 1))
+    fi
+    # bm_NTHREADS=$((${gem5_nCores} - 1))
     printf "Benchmark Threads = ${bm_NTHREADS} \n"
 
     # Find .runconf
@@ -77,7 +89,7 @@ for bm_name in "${bm_names[@]}"; do
 
     gem5_out_dir="${gem5_dir}/${gem5_result_prefix}/m5out-${gem5_test_case}/${bm_name}"
     rm -rf ${gem5_out_dir}
-    mkdir ${gem5_out_dir} -v
+    mkdir -p ${gem5_out_dir} -v
 
     # Write Config to file
     cfg_file="${bm_name}.gem5conf"
@@ -99,20 +111,22 @@ for bm_name in "${bm_names[@]}"; do
     # echo ${EXEC_CMD}
     # eval ${EXEC_CMD}
 
-    bm_hw_cmd="${gem5_dir}/benchmarks/parsec-2.1/bin/parsecmgmt -a run -p ${bm_name} -i ${bm_dataset} -x pre"
+    bm_hw_cmd="${gem5_dir}/../benchmarks/parsec-2.1/bin/parsecmgmt -a run -p ${bm_name} -i ${bm_dataset} -x pre"
     eval ${bm_hw_cmd}
 
     cd ${run_path}
 
 
     bm_args="${run_args}"
-    gem5_cmd="${gem5_dir}/${gem5_opt} -d ${gem5_out_dir} ${gem5_args}"
+    # gem5_cmd="${gem5_opt} --debug-flags=ProtocolTrace -d ${gem5_out_dir} ${gem5_args}"
+    gem5_cmd="${gem5_opt} -d ${gem5_out_dir} ${gem5_args}"
     exec_cmd="${gem5_cmd} -c ${bm_run_exec} -o \"${run_args}\""
     echo ${exec_cmd}
+    # eval "${exec_cmd}"
     eval "nohup ${exec_cmd} &"
 
-    debug_cmd="gdb --args ${gem5_dir}/${gem5_opt} -d ${gem5_out_dir} ${gem5_args} -c ${bm_run_exec} -o \"${run_args}\""
-    # echo ${debug_cmd}
+    debug_cmd="gdb --args ${gem5_opt} -d ${gem5_out_dir} ${gem5_args} -c ${bm_run_exec} -o \"${run_args}\""
+    # echo ${debug_cmd} 
     # eval ${debug_cmd}
 
 done
